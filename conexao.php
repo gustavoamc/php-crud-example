8cb512ff4c8e7385bed96ac9a81dd4dfecf8ea61
Gustavo A M Costa <51254837+gustavoamc@users.noreply.github.com>
2024-01-29 10:21:09 -0300

    adicionei comentarios a conexao

--- conexao.php
+++ conexao.php
@@ -4,8 +4,18 @@ $usuario = "root";
 $senha = "";
 $banco = "usutest";
 
-//$conexao = new mysqli($host, $usuario, $senha, $banco); //works
-
 $conexao = new PDO('mysql:host=127.0.0.1;dbname=usutest;port=3306','root','');
 
+/*sql query usada para gerar o db e a tabela
+
+CREATE DATABASE usutest;
+
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30),
+    email VARCHAR(30),
+);
+
+*/
 ?>
+
